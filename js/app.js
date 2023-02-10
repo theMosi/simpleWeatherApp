418b102d8f3b48be3db9129c3ff6687d5683bd18
Mosi <mostafawshe@gmail.com>
2023-02-10 05:10:53 +0330

    make it Functional

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 console.log('Naya Injoor JaHa');
 let father = document.querySelector('body');
 
+
 let wBtn = document.querySelector('#w-change-btn');
 let cName = document.querySelector('#city');
+let cNameValue = 'London';
 
 let locationElem = document.querySelector('#w-location');
 let wMainElem = document.querySelector('#w-main');
@@ -17,31 +19,66 @@ let lonElem = document.querySelector('#w-lon');
 let latElem = document.querySelector('#w-lat');
 
 
-let getWeather = async () => {
 
-    let cNameValue = cName.value;
+let loadFunc = () => {
+    let curCity = getLocal();
+    if (curCity === null) {
+        cNameValue = 'London';
+        setDOMFunc();
+    } else {
+        cNameValue = curCity;
+        setDOMFunc();
 
-
-    let wData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cNameValue}&appid=281491e8a066d857590e3ef0c1b0a679`);
-    let wDataParse = await wData.json();
-
-    locationElem.innerHTML = cNameValue;
-    wMainElem.innerHTML = wDataParse.weather[0].main;
-    iconElem.setAttribute('src', `http://openweathermap.org/img/wn/${wDataParse.weather[0].icon}@2x.png`)
-    tempElem.innerHTML = wDataParse.main.temp;
-    maxTempElem.innerHTML = wDataParse.main.temp_max;
-    minTempElem.innerHTML = wDataParse.main.temp_min;
-    pressureElem.innerHTML = wDataParse.main.pressure;
-    humidityElem.innerHTML = wDataParse.main.humidity;
-    windElem.innerHTML = wDataParse.wind.speed;
-    lonElem.innerHTML = wDataParse.coord.lon;
-    latElem.innerHTML = wDataParse.coord.lat;
-
-    $('#locationModal').modal('hide');
-
-    console.log(wDataParse)
+    }
 
 }
 
 
-wBtn.addEventListener('click', getWeather);
+let getWeather = async (wValue) => {
+    let wData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${wValue}&appid=281491e8a066d857590e3ef0c1b0a679`);
+    let wDataParse = await wData.json();
+
+    return wDataParse;
+}
+
+
+let setDOMFunc = async () => {
+
+    if (cName.value !== '') {
+        cNameValue = cName.value;
+    }
+
+    const wData = await getWeather(cNameValue)
+
+    locationElem.innerHTML = cNameValue;
+    wMainElem.innerHTML = wData.weather[0].main;
+    iconElem.setAttribute('src', `http://openweathermap.org/img/wn/${wData.weather[0].icon}@2x.png`)
+    tempElem.innerHTML = wData.main.temp;
+    maxTempElem.innerHTML = wData.main.temp_max;
+    minTempElem.innerHTML = wData.main.temp_min;
+    pressureElem.innerHTML = wData.main.pressure;
+    humidityElem.innerHTML = wData.main.humidity;
+    windElem.innerHTML = wData.wind.speed;
+    lonElem.innerHTML = wData.coord.lon;
+    latElem.innerHTML = wData.coord.lat;
+
+    $('#locationModal').modal('hide');
+
+    setLocal(cNameValue);
+}
+
+
+
+let setLocal = (setValue) => {
+    localStorage.setItem('city', setValue);
+}
+
+
+let getLocal = () => {
+    return localStorage.getItem('city');
+}
+
+
+
+document.addEventListener('DOMContentLoaded', loadFunc);
+wBtn.addEventListener('click', setDOMFunc);
